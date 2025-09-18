612f7e35d8e4949fd4ef74b96cb308af268df7f9
Renatinho <renatoboss001@gmail.com>
2025-09-18 17:30:45 -0300

    Mudando de insert para upsert

--- main.js
+++ main.js
@@ -38,11 +38,27 @@ const getProdutos = async () => {
     //adicionar itens no banco
     console.log('Vamos inserir os dados na tabela 🙃');
 
+    const cols = [
+        'cod_produto_final',
+        'nome_produto_final',
+        'volumetria_ml',
+        'volumetria_pct_cx',
+        'qntd_por_pct_cx',
+        'linha',
+        'custo_prod',
+        'custo_mp',
+        'custo_emb',
+        'embalagem_tipo',
+        'ativo'
+    ]
+
     for(const row of rows){
         if(isNaN(row['Cod. Produto Final'])) continue;
         try{
             await pool.query(
-                "insert into bd_produtos values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10,$11)",
+                "insert into bd_produtos values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10,$11)\n" +
+                "on conflict (cod_produto_final)\n"+
+                `do update set(${cols.join(',')}) = ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10,$11)`,
                 [
                     row['Cod. Produto Final'],
                     row['Nome Produto Final'],
@@ -98,6 +114,42 @@ const getProducao = async () => {
 
     console.log('inserindo os dados ☺️');
 
+    const cols = [
+        'id',
+        'data',
+        'linha',
+        'lote',
+        'sku',
+        'nome_produto',
+        'volumetria_ml',
+        'qntd_pct_cx',
+        'producao_planejada',
+        'producao_real',
+        'hora_inicio_producao',
+        'hora_fim_producao',
+        'tempo_disp_producao',
+        'tempo_inatividade',
+        'total_producao',
+        'perca_rot',
+        'perca_tampa',
+        'perca_garrafa_lata_vazia',
+        'perca_garrafa_lata_cheia',
+        'perca_preforma',
+        'perca_filmeliso',
+        'perca_filmestrech',
+        'perca_filmeimpresso',
+        'minimo',
+        'turno',
+        'lider',
+        'lts_produzidos',
+        'custo_prod',
+        'custo_xarope',
+        'custo_emb',
+        'custo_prod_total',
+        'custoemb_total',
+        'customp_total',
+    ]
+
     for(const r of rows){
 
 
@@ -110,7 +162,8 @@ const getProducao = async () => {
             row[13] = row[13].toISOString().slice(11, 19);
             row[14] = 25;
             await pool.query(
-                "insert into bd_producao values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33)",
+                `insert into bd_producao (${cols.join(',')}) values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33)\n`
+                +`on conflict (id) do update set (${cols.join(',')}) = ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33)`,
                 row
             )
         }
